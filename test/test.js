23d6f455a5ecd0e1d346aaeb6d3bdebb1724c58a
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2016-07-02 02:21:05 -0400

    perf boost by droping color-difference

--- test/test.js
+++ test/test.js
@@ -18,13 +18,11 @@ describe('test differe posterize inputs on a photo of obama', function() {
   });
 
   it('converts an image into a poster', function() {
-    this.timeout(4500);
     var out = posterize(img, 3);
     savePixels(out, 'jpg').pipe(fs.createWriteStream(path.join(__dirname, 'output', Date.now()+'.jpg')));
   });
 
   it('converts an image into a poster with your colors', function() {
-    this.timeout(4500);
     var out = posterize(img, [
       {red: 252, green: 228, blue: 168},
       {red: 215, green: 26, blue: 33},
